36cb818527ad0aef814470f2a114497332ffda55
Npx <cheyprivado@gmail.com>
2023-03-23 20:01:26 -0300

    :bulb: Atualizando comentários

--- inscricao.py
+++ inscricao.py
@@ -21,6 +21,7 @@ if student.age >= 18:
 else:
   authorized = input("Você possui autorização dos responsáveis para participar? S - SIM ou N - NÃO: ")
 
+  #verificando se o estudante tem autorização dos responsáveis
   if authorized.upper() == 'S':
       print(f"\n{student.name.split()[0]}, sua participação foi autorizada.")
       print("Mais instruções serão enviadas ao seu e-mail cadastrado na FIAP!")
