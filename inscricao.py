df22d84ba50496abe54c8eebc852eb9fb4aef2cb
Npx <cheyprivado@gmail.com>
2023-03-23 19:42:05 -0300

    :hammer: Atualizando script

--- inscricao.py
+++ inscricao.py
@@ -1,16 +1,28 @@
 print("\n   FIAP Esportes - Inscrição\n")
 
-em = int(input("Por favor, digite seu RM: "))
-age = int(input("Por favor, digite sua idade: "))
+class StudentInfo():
+   em = int(input("Por favor, digite seu RM: "))
+   name = input("Por favor, digite seu nome completo: ")
+   age = int(input("Por favor, digite sua idade: "))
 
-if age >= 18:
-  print(f"\nSua participação foi autorizada, aluno de RM: {em}")
+student = StudentInfo()
+
+# verificando tamanho do rm
+if len(str(student.em)) is not 6:
+   print("\nRM invalido, tente novamente.")
+
+   quit()
+
+# verificando se o estudante tem mais de 18 anos
+if student.age >= 18:
+  print(f"\n{student.name.split()[0]}, sua participação foi autorizada.")
   print("Mais instruções serão enviadas ao seu e-mail cadastrado na FIAP!")
+  
 else:
   authorized = input("Você possui autorização dos responsáveis para participar? S - SIM ou N - NÃO: ")
 
-  if authorized == 'S':
-      print(f"\nSua participação foi autorizada, aluno de RM: {em}")
+  if authorized.upper() == 'S':
+      print(f"\n{student.name.split()[0]}, sua participação foi autorizada.")
       print("Mais instruções serão enviadas ao seu e-mail cadastrado na FIAP!")
 
   else:
